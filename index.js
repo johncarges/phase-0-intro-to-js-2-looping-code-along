25dc82fdfca02b76ed845cd6964ca8982e30a517
John Carges <johncarges@gmail.com>
2023-06-13 23:54:39 -0400

    passes all tests and adds logs (commented out)

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(listOfNames, typeOfGift) {
+    const thankYouCards = []
+    for(let index = 0; index <listOfNames.length; index++){
+        thankYouCards.push(`Thank you, ${listOfNames[index]}, for the wonderful ${typeOfGift} gift!`)
+        
+        console.log(thankYouCards)
+    }
+    return thankYouCards
+}
+
+// console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+function countDown(startingNumber) {
+    for(let index = startingNumber; index >= 0; index--){
+        console.log(index)
+    }
+}
+
+// countDown(10)
